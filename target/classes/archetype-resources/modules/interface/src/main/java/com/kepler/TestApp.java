82fe44e6b6b539daf237111fe96cb90554ba70e2
shenjiawei <shenjiawei@didichuxing.com>
2016-03-08 14:47:50 +0800

    删除Target目录

--- target/classes/archetype-resources/modules/interface/src/main/java/com/kepler/TestApp.java
+++
@@ -1,8 +0,0 @@
-package com.kepler;
-import com.kepler.annotation.Service;
-
-@Service(version = "0.0.1-${artifactId}")
-public interface TestApp {
-
-	public String test(String kepler) throws Exception;
-}
